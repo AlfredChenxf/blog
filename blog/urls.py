e79098f9ea9f92149ca61b1eca703c6aaa461d19
AlfredChenxf <alexalfred@163.com>
2015-03-26 14:51:03 +0800

    Add hours_ahead function

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from firstBlog.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^blog/$', 'firstBlog.views.blog'),
+    url(r'^latest/$', latest_book),
+    url(r'^blog/$', blog),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^admin/', include(admin.site.urls)),
 )
--- firstBlog/views.py
+++ firstBlog/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from models import book
+import datetime
 
 # Create your views here.
 def blog(request):
-    return HttpResponse('<h1>Hello Alfred<h1>')
+    return HttpResponse('<h1>Hello Alfred, it is now: %s</h1>' % datetime.datetime.now())
+
+def latest_book(request):
+    book_list = book.objects.order_by('-pub_data')[:10]
+    return render_to_response('latest_books.html', {'book_list': book_list})
+
+def hours_ahead(request, offset):
+    # offset = int(offset)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body><h1>In %s hour(s), it will be %s.</h1></body></html>" % (offset, dt)
+    return HttpResponse(html)
